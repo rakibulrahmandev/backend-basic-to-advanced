0840b4ef6462c61d5717731da9c106c39b2e5824
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-09-09 12:55:08 +0600

    backend chapter-3 / node modules and npm

---
+++ backend-chapter-3/index.js
@@ -0,0 +1,11 @@
+//* NPM understanding:
+/**
+ *  Installing and uninstalling anything basic and advanced.
+ *  understanding node_modules.
+ *  dependencies
+ *  scripts- understanding default scripts path and custom scripts.
+*/
+
+//! installing and uninstalling:
+// -> npm install "package name";
+// -> npm uninstall "package name";
