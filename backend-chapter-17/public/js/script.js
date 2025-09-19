4bff8a1d7944941d05b762e6b190c923e11fe0f8
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-09-19 21:36:57 +0600

    backend chapter-17 / data association, mini project part-2

---
+++ backend-chapter-17/public/js/script.js
@@ -0,0 +1,14 @@
+const input = document.getElementById('input');
+const toggleBtn = document.getElementById('toggle_btn');
+
+toggleBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (input.type === 'password') {
+        input.type = 'text';
+        toggleBtn.innerHTML = '<i class="fa-solid fa-eye text-gray-500/20"></i>';
+    } else if (input.type === 'text') {
+        input.type = 'password';
+        toggleBtn.innerHTML = '<i class="fa-solid fa-eye-slash text-gray-500/20"></i>'
+    };
+});
